b255685fae66b37149c2e862563c24322c36e0fd
SGarcia710 <sgarcia710@outlook.com>
2020-03-14 16:33:28 -0500

    Template finished

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,12 @@ import dot from "./assets/img/dot.svg";
 
 import bg from "./assets/img/bg3.svg";
 
-import { ProjectsData, ThoughtsData } from "./assets/data/Data";
+import {
+  ProjectsData,
+  ThoughtsData,
+  TeamData,
+  Partners
+} from "./assets/data/Data";
 
 class App extends Component {
   constructor(props) {
@@ -78,6 +83,34 @@ class App extends Component {
             </div>
             <div className="LandingPageButton">SEE ALL WORK</div>
           </div>
+          <div className="LandingPage--team">
+            <div className="TeamTitle">OUR TEAM</div>
+            <div className="TeamSeparator" />
+            <div className="TeamCarousel">
+              <div className="TeamCarousel--body">
+                {TeamData.map((member, i) => {
+                  if (i < 5) {
+                    return (
+                      <div className="TeamCarousel--body__slide">
+                        <img
+                          src={member.image}
+                          alt=""
+                          className={i === 2 ? "SlideImageBig" : "SlideImage"}
+                        />
+                        <div className="SlideTitle">
+                          {i === 2 ? member.name : <br />}
+                        </div>
+                        <div className="SlideDescription">
+                          {i === 2 ? member.position : <br />}
+                        </div>
+                      </div>
+                    );
+                  }
+                  return <> </>;
+                })}
+              </div>
+            </div>
+          </div>
           <div className="LandingPage--thoughts">
             <div className="ThoughtsTitle">OUR THOUGHTS</div>
             <div className="ThoughtsSeparator" />
@@ -101,6 +134,135 @@ class App extends Component {
                   }
                   return <> </>;
                 })}
+              </div>
+            </div>
+          </div>
+          <div className="LandingPage--letsTalk">
+            <div className="LandingPage--letsTalk__title">
+              LET’S TALK TOGETHER
+            </div>
+            <div className="LandingPage--letsTalk__text">
+              Speaking comes to most people as naturally as breathing. On many
+              occasions our words are uttered without conscious thought; in fact
+              we rarely stop
+            </div>
+            <div className="LandingPage--letsTalk__button">
+              <button className="LandingPageButton">GET STARTED NOW</button>
+            </div>
+          </div>
+          <div className="LandingPage--footer">
+            <div className="LandingPage--footer__partners">
+              {Partners.map(partner => {
+                return (
+                  <img
+                    className="partnerLogo"
+                    src={partner.logo}
+                    alt={partner.name}
+                  />
+                );
+              })}
+            </div>
+            <div className="LandingPage--footer__links">
+              <div className="linksColumn">
+                <div className="linksColumn--title">OUR THOUGHTS</div>
+
+                <div className="linksColumn--body">
+                  <div className="linksColumn--body__item">
+                    <div className="link">
+                      E Banks That Accept Us Casino Players
+                    </div>
+                    <div className="date">24 Apr 2018</div>
+                  </div>
+                  <div className="linksColumn--body__item">
+                    <div className="link">
+                      Little Things Do Make A Difference
+                    </div>
+                    <div className="date">19 Oct 2018</div>
+                  </div>
+                  <div className="linksColumn--body__item">
+                    <div className="link">
+                      Guidelines For Inkjet Cartridge Refill
+                    </div>
+                    <div className="date">20 Jul 2018</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="linksColumn">
+                <p className="linksColumn--title">CONTACT US</p>
+
+                <div className="linksColumn--body">
+                  <div className="linksColumn--body__detail_item">
+                    <div className="title">Phone number</div>
+                    <div className="info">+44 123 456 789</div>
+                  </div>
+                  <div className="linksColumn--body__detail_item">
+                    <div className="title">Email</div>
+                    <div className="info">hello@your-domain.com</div>
+                  </div>
+                  <div className="linksColumn--body__detail_item">
+                    <div className="title">Business hours</div>
+                    <div className="info">Monday - Friday 10 AM - 9 PM</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="linksColumn">
+                <div className="linksColumn--title">USEFUL LINKS</div>
+
+                <div className="linksColumn--body">
+                  <div className="linksColumn--body__item">
+                    <div className="link">About Us</div>
+                  </div>
+                  <div className="linksColumn--body__item">
+                    <div className="link">Our Work</div>
+                  </div>
+                  <div className="linksColumn--body__item">
+                    <div className="link">Contact Us</div>
+                  </div>
+                  <div className="linksColumn--body__item">
+                    <div className="link">Video Tutorial</div>
+                  </div>
+                  <div className="linksColumn--body__item">
+                    <div className="link">Licenses</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="linksColumn">
+                <div className="linksColumn--title">USEFUL LINKS</div>
+
+                <div className="linksColumn--body">
+                  <div className="linksColumn--body__item">
+                    <div className="link">Help Center</div>
+                  </div>
+                  <div className="linksColumn--body__item">
+                    <div className="link">Support Forum</div>
+                  </div>
+                  <div className="linksColumn--body__item">
+                    <div className="link">Online Documentation</div>
+                  </div>
+                  <div className="linksColumn--body__item">
+                    <div className="link">Video Tutorial</div>
+                  </div>
+                  <div className="linksColumn--body__item">
+                    <div className="link">Licenses</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="LandingPage--footer__foot">
+              <div className="signature">
+                © 2020 - Develop with
+                <span role="img" aria-label="heart">
+                  ♥️
+                </span>
+                by @SGarcia710 | Designed by Tran Mau Tri Tam
+              </div>
+              <div className="socialIcons">
+                <Icon.Twitter />
+                <Icon.GitHub />
+                <Icon.Linkedin />
               </div>
             </div>
           </div>
--- src/assets/data/Data.jsx
+++ src/assets/data/Data.jsx
@@ -6,6 +6,33 @@ import OurThoughtsOne from "../img/OurThoughtsOne.jpg";
 import OurThoughtsTwo from "../img/OurThoughtsTwo.jpg";
 import OurThoughtsThree from "../img/OurThoughtsThree.jpg";
 
+import TeamMemberOne from "../img/TeamMemberOne.jpg";
+import TeamMemberTwo from "../img/TeamMemberTwo.jpg";
+import TeamMemberThree from "../img/TeamMemberThree.jpg";
+import TeamMemberFour from "../img/UXLeader.jpeg";
+import TeamMemberFive from "../img/Developer1.jpeg";
+import TeamMemberSix from "../img/CommunityManager.jpeg";
+
+import CesisLogo from "../img/Cesis.svg";
+import BehanceLogo from "../img/Behance.svg";
+import AdidasLogo from "../img/Adidas.svg";
+import NikeLogo from "../img/Nike.svg";
+
+export const Partners = [
+  {
+    name: "Cesis",
+    logo: CesisLogo
+  },
+  {
+    name: "Behance",
+    logo: BehanceLogo
+  },
+
+  {
+    name: "Nike",
+    logo: NikeLogo
+  }
+];
 export const ProjectsData = [
   {
     image: ProjectOneImg,
@@ -23,7 +50,95 @@ export const ProjectsData = [
     description: "App Design, UX/UI"
   }
 ];
-
+export const TeamData = [
+  {
+    image: TeamMemberOne,
+    name: "Mike Fisher",
+    position: "CEO",
+    academics: [
+      {
+        title: "Electrical Engineering",
+        site: "Universidad Santiago de Cali",
+        year: "2014"
+      },
+      {
+        title: "Master of Science in Statistics",
+        site: "Universidad Autonoma de Occidente",
+        year: "2017"
+      }
+    ]
+  },
+  {
+    image: TeamMemberTwo,
+    name: "Ryan Scott",
+    position: "CTO",
+    academics: [
+      {
+        title: "Computer Science",
+        site: "MIT",
+        year: "2015"
+      },
+      {
+        title: "Master of Computer Science",
+        site: "MIT",
+        year: "2018"
+      }
+    ]
+  },
+  {
+    image: TeamMemberThree,
+    name: "Robert Young",
+    position: "Project Management",
+    academics: [
+      {
+        title: "Business Administration",
+        site: "Universidad Santiago de Cali",
+        year: "2008"
+      },
+      {
+        title: "Product Owner",
+        site: "ScrumColombia",
+        year: "2016"
+      }
+    ]
+  },
+  {
+    image: TeamMemberFour,
+    name: "Emma Gutierrez",
+    position: "UX/UI Leader",
+    academics: [
+      {
+        title: "Interactive Media Designer",
+        site: "Universidad ICESI",
+        year: "2010"
+      }
+    ]
+  },
+  {
+    image: TeamMemberFive,
+    name: "Gussie Moreno",
+    position: "Backend Leader",
+    academics: [
+      {
+        title: "Systems Information Engineering",
+        site: "Universidad Santiago de Cali",
+        year: "2014"
+      }
+    ]
+  },
+  {
+    image: TeamMemberSix,
+    name: "Gussie Moreno",
+    position: "Community Manager",
+    academics: [
+      {
+        title: "Advertising & Public Relations",
+        site: "University of Alabama",
+        year: "2016"
+      }
+    ]
+  }
+];
 export const ThoughtsData = [
   {
     image: OurThoughtsOne,
